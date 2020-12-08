70c1a585710207a39a7a8c797bc2f1e8c6408d63
Liz Loftus <65726009+lizzydeb233@users.noreply.github.com>
2020-12-08 18:15:17 -0500

    Check My Work

---
+++ js/ratings.js
@@ -0,0 +1,14 @@
+function collectratings(params) {
+    let ratings = {
+        count : 0,
+        sum : 0,
+        average : 0
+    };
+    let rating = 0;
+};
+
+let elements = document.querySelectorAll("rating");
+
+elements.forEach(function(element){
+
+});
