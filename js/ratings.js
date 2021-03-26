9439d2c0d6d10e166e52c98e172f6918a639db72
Liz Loftus <65726009+lizzydeb233@users.noreply.github.com>
2021-03-26 14:28:20 -0400

    restarting this one...

--- js/ratings.js
+++ js/ratings.js
@@ -1,14 +1,4 @@
-function collectratings(params) {
-    let ratings = {
-        count : 0,
-        sum : 0,
-        average : 0
-    };
+function collect_ratings(){
+    let ratings = {count:0, sum:0, average:0};
     let rating = 0;
-};
-
-let elements = document.querySelectorAll("rating");
-
-elements.forEach(function(element){
-
-});
+}
